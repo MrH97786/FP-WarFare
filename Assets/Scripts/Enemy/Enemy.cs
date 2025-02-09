df439b5be0959f095bd0be473005c5f8b49416dc
Haroon Hafiz <haroon97786@gmail.com>
2025-02-09 17:19:10 +0000

    Added basic detection system to enemy
    The detection system is very basic so far, all it does is detect a player and sets a ray cast to that player. The enemy does no do anything else so far

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -8,9 +8,14 @@ public class Enemy : MonoBehaviour
     private StateController stateController;
     private NavMeshAgent agent;
     public NavMeshAgent Agent {get => agent;}
+
     [SerializeField]
     private string currentState;
     public EnemyPath enemyPath;
+
+    private GameObject player;
+    public float detectionRange = 20f;
+    public float fieldOfView = 85f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +23,30 @@ public class Enemy : MonoBehaviour
         stateController = GetComponent<StateController>();
         agent = GetComponent<NavMeshAgent>();
         stateController.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DetectPlayer();
+    }
+
+    public bool DetectPlayer()
+    {
+        if (player != null)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) < detectionRange)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position;
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if (angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView)
+                {
+                    Ray ray = new Ray(transform.position, targetDirection);
+                    Debug.DrawRay(ray.origin, ray.direction * detectionRange);
+                }
+            }
+        }
+        return true;
     }
 }
