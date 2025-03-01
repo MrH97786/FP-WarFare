dbc6f369657ac0e9cb8212de09ae072dca1db3c7
Haroon Hafiz <haroon97786@gmail.com>
2025-03-01 18:06:03 +0000

    Improved Weapon functionality
    Added some settings to the player weapon class. The gun has three settings; single, auto and burst. Single shoots one bullet, auto shoots many bullets in quick succession and burst shoots 3 bullets in a spread. Also added shooting delay to control the delay after each shot which will depend on the gun.

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -4,38 +4,127 @@ using UnityEngine.InputSystem;
 
 public class PlayerWeapon : MonoBehaviour
 {
+    public Camera camera;
+
+    public bool isShooting;
+    public bool readyToShoot;
+    bool allowReset = true;
+    public float shootingDelay = 2f;
+
+    // For weapon Burst
+    public int bulletsPerBurst = 3;
+    public int currentBurst;
+
+    // For weapon Spread
+    public float spreadIntensity;
+
+    // Bullet
     public Transform bulletSpawn;
     public float bulletVelocity = 40;
     public float bulletLifeTime = 3f;
     private PlayerInput playerInput;
     private InputAction fireAction;
 
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    public ShootingMode currentShootingMode;
+
     void Awake()
     {
         // Initialize the PlayerInput and get the Fire action from the OnFoot input map
         playerInput = new PlayerInput();
         fireAction = playerInput.OnFoot.Fire;
+
+        readyToShoot = true;
+        currentBurst = bulletsPerBurst;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fireAction.triggered)
+        // Read the Fire action state
+        bool firePressed = fireAction.ReadValue<float>() > 0;  // InputSystem uses float values (0 or 1) for actions
+
+        if (currentShootingMode == ShootingMode.Auto)
         {
+            isShooting = firePressed;  // Holding down button
+        }
+        else if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            isShooting = fireAction.triggered; // Single press
+        }
+
+        if (readyToShoot && isShooting)
+        {
+            currentBurst = bulletsPerBurst;
             FireWeapon();
         }
     }
 
     private void FireWeapon()
     {
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+
         // Create new bullet
         GameObject bullet = GameObject.Instantiate(Resources.Load("Prefabs/Bullet") as GameObject, bulletSpawn.position, Quaternion.identity);
 
+        bullet.transform.forward = shootingDirection;
+
         // Shoot the bullet and add force to the bullet
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+        bullet.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletVelocity, ForceMode.Impulse);
 
         // Destroy bullet after a couple seconds
         StartCoroutine(DestroyBullet(bullet, bulletLifeTime));
+
+        // Quick check when done shooting
+        if (allowReset)
+        {
+            Invoke("ResetShot", shootingDelay);
+            allowReset = false;
+        }
+
+        // Burst Mode
+        if (currentShootingMode == ShootingMode.Burst && currentBurst > 1)
+        {
+            currentBurst--;
+            Invoke("FireWeapon", shootingDelay);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else 
+        {
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        float x = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+        float y = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+
+        return direction + new Vector3(x, y, 0);
     }
 
     private IEnumerator DestroyBullet(GameObject bullet, float delay)
