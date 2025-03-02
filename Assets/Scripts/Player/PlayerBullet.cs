70001a98c3c6400db2f014fec34f5985cfc3feac
Haroon Hafiz <haroon97786@gmail.com>
2025-03-02 13:03:07 +0000

    Added effects package, hole image and bullet hole functionality
    Added effects package,
    Added a hole image for bullet,
    Bullet hole functionality done. bullet hits an object and smoke affect with image of hole appears

--- Assets/Scripts/Player/PlayerBullet.cs
+++ Assets/Scripts/Player/PlayerBullet.cs
@@ -10,7 +10,22 @@ public class PlayerBullet : MonoBehaviour
         if (hitTransform.CompareTag("Target"))
         {
             Debug.Log("Hit " + collision.gameObject.name + " !");
+            CreateBulletImpactEffect(hitTransform, collision);
         }
+        
+        if (hitTransform.CompareTag("Wall") || hitTransform.CompareTag("Floor"))
+        {
+            Debug.Log("Hit a wall or floor!");
+            CreateBulletImpactEffect(hitTransform, collision);
+        }
+
         Destroy(gameObject);
+    }
+
+    void CreateBulletImpactEffect(Transform objectWeHit, Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        GameObject hole = Instantiate(GlobalReferences.Instance.bulletImpactEffect, contact.point, Quaternion.LookRotation(contact.normal));
+        hole.transform.SetParent(objectWeHit);
     }
 }
