68d5e450ddfea3492b216afa9b10b34cc8707c16
Haroon Hafiz <haroon97786@gmail.com>
2025-03-17 16:58:29 +0000

    Added level complete functionality
    Once player reaches the teleporter after unlocking the gate. it gives the player a level complete message and blacks out to the main menu

---
+++ Assets/Scripts/TeleporterPad.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TeleporterPad : MonoBehaviour
+{
+    public GameObject LevelCompleteUI;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<PlayerScreenBlackout>().StartFade();
+            LevelCompleteUI.gameObject.SetActive(true);
+
+            int waveSurvived = GlobalReferences.Instance.waveNumber;
+
+            if (waveSurvived - 1 > SaveLoadManager.Instance.LoadHighScore())
+            {
+                SaveLoadManager.Instance.SaveHighScore(waveSurvived - 1);
+            }
+
+            StartCoroutine(ReturnToMainMenu(4f));
+        }
+    }
+
+
+    private IEnumerator ReturnToMainMenu(float delayBeforeReturn)
+    {
+        // Wait for a delay (e.g., to show the level complete message)
+        yield return new WaitForSeconds(delayBeforeReturn);
+
+        // Load the Main Menu scene
+        SceneManager.LoadScene("MainMenu");
+    }
+}
